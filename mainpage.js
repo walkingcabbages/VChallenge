40b1604dc03034dec36c8139cc0f2a4dff0b91d9
Amy Cheng <walkingcabbages@Amys-MacBook.local>
2019-05-21 22:44:00 -0700

    starting point for react-native using expo and apollo-client

---
+++ mainpage.js
@@ -0,0 +1,38 @@
+{/*import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+*/}
+
+{/*const getGiphQuery = gql`
+	{
+		giphy {
+			random(tag: "javascript", rating: pg) {
+				id
+				url
+				images {
+					original {
+						url
+					}
+				}
+			}
+		}
+	}
+`*/}
+{/*
+class MainPage extends React.Component {
+
+  render() {
+  	console.log("reached mainpage");
+  	return (
+  		<ul id='giphyLinks'>
+  			<li>WORK PLZ</li>
+  		</ul>
+	);
+  }
+}
+*/}
+
+{/*export default graphql(getGiphQuery)(MainPage);*/}
+
+
